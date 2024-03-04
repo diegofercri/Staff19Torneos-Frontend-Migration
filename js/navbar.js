cf5b7daed7bbdaae6ebc4dadda856ba5f99bc87d
Diego Fernández Criado <dfc25@icloud.com>
2024-03-04 13:16:04 +0100

    navbar migration complete

--- js/navbar.js
+++ js/navbar.js
@@ -1,63 +1,63 @@
-const nav = document.getElementById('nav');
+const navbarStaff = document.getElementById('navbarStaff');
 const toggleMenuOpen = document.getElementById('toggle-menu__open');
 const toggleMenuClose = document.getElementById('toggle-menu__close');
 const mainMenu = document.getElementById('main-menu');
 
 toggleMenuOpen.addEventListener('click', e => {
-    nav.classList.toggle('nav--show');
+    navbarStaff.classList.toggle('navbarStaff--show');
     mainMenu.classList.toggle('main-menu--show');
 });
 
 toggleMenuClose.addEventListener('click', e => {
-    nav.classList.toggle('nav--show');
+    navbarStaff.classList.toggle('navbarStaff--show');
     mainMenu.classList.toggle('main-menu--show');
 });
 
 
-const dropdownList0 = document.getElementById('dropdown-list0');
-const dropdownToggle0 = document.getElementById('dropdown-toggle0');
-const dropdownList1 = document.getElementById('dropdown-list1');
-const dropdownToggle1 = document.getElementById('dropdown-toggle1');
+const dropdownStaffList0 = document.getElementById('dropdownStaff-list0');
+const dropdownStaffToggle0 = document.getElementById('dropdownStaff-toggle0');
+const dropdownStaffList1 = document.getElementById('dropdownStaff-list1');
+const dropdownStaffToggle1 = document.getElementById('dropdownStaff-toggle1');
 
-dropdownToggle0.addEventListener('click', e => {
-    dropdownList0.classList.toggle('dropdown-list--show');
-    if (dropdownList1.classList.contains('dropdown-list--show-1')) {
-        dropdownList1.classList.remove('dropdown-list--show-1');
-        dropdownToggle1.querySelector('.fa-angle-down').classList.toggle('rotated');
+dropdownStaffToggle0.addEventListener('click', e => {
+    dropdownStaffList0.classList.toggle('dropdownStaff-list--show');
+    if (dropdownStaffList1.classList.contains('dropdownStaff-list--show-1')) {
+        dropdownStaffList1.classList.remove('dropdownStaff-list--show-1');
+        dropdownStaffToggle1.querySelector('.fa-angle-down').classList.toggle('rotated');
     }
-    dropdownToggle0.querySelector('.fa-angle-down').classList.toggle('rotated');
+    dropdownStaffToggle0.querySelector('.fa-angle-down').classList.toggle('rotated');
     imgCloseChecker();
 });
 
-dropdownToggle1.addEventListener('click', e => {
-    dropdownList1.classList.toggle('dropdown-list--show-1');
-    if (dropdownList0.classList.contains('dropdown-list--show')) {
-        dropdownList0.classList.remove('dropdown-list--show');
-        dropdownToggle0.querySelector('.fa-angle-down').classList.toggle('rotated');
+dropdownStaffToggle1.addEventListener('click', e => {
+    dropdownStaffList1.classList.toggle('dropdownStaff-list--show-1');
+    if (dropdownStaffList0.classList.contains('dropdownStaff-list--show')) {
+        dropdownStaffList0.classList.remove('dropdownStaff-list--show');
+        dropdownStaffToggle0.querySelector('.fa-angle-down').classList.toggle('rotated');
     }
-    dropdownToggle1.querySelector('.fa-angle-down').classList.toggle('rotated');
+    dropdownStaffToggle1.querySelector('.fa-angle-down').classList.toggle('rotated');
     imgCloseChecker();
 });
 
 function imgCloseChecker() {
     // Verifica si ambos toggles están activados
-    if (dropdownList0.classList.contains('dropdown-list--show') || dropdownList1.classList.contains('dropdown-list--show-1')) {
+    if (dropdownStaffList0.classList.contains('dropdownStaff-list--show') || dropdownStaffList1.classList.contains('dropdownStaff-list--show-1')) {
         // Asegura que la clase esté presente para ocultar la imagen
-        nav.classList.add('nav-logo--hidden');
+        navbarStaff.classList.add('navbarStaff-logo--hidden');
         setTimeout(function () {
-            nav.classList.add('nav-logo--hidden2');
+            navbarStaff.classList.add('navbarStaff-logo--hidden2');
         }, 500)
     } else {
         // Asegura que la clase esté ausente para mostrar la imagen
-        nav.classList.remove('nav-logo--hidden');
-        nav.classList.remove('nav-logo--hidden2');
+        navbarStaff.classList.remove('navbarStaff-logo--hidden');
+        navbarStaff.classList.remove('navbarStaff-logo--hidden2');
     }
 }
 
 
 window.addEventListener('resize',() => {
-    if (nav.classList.contains('nav--show')) {
-        nav.classList.remove('nav--show');
+    if (navbarStaff.classList.contains('navbarStaff--show')) {
+        navbarStaff.classList.remove('navbarStaff--show');
         mainMenu.classList.remove('main-menu--show');
     }
 });
